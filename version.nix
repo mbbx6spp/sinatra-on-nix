0a1dae8b114ee4f5bdf81f3914a1e1b63d98f4b8
Susan Potter <me@susanpotter.net>
2016-11-22 10:44:48 -0600

    Update with fully bootstrapped shell.nix and s/siege/wrk/

---
+++ version.nix
@@ -0,0 +1,5 @@
+{
+  name = "nixos";
+  url = "https://d3g5gsiof5omrk.cloudfront.net/nixos/16.09/nixos-16.09.680.4e14fd5/nixexprs.tar.xz";
+  sha256 = "0dj8xzlj0lc4vsf3qp3cmkq2qjkrj2s2frnsi59j22iw0q7srzk5";
+}
